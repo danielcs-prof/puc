84ffc92a20d75fa66b9e07843e3911b82aaada87
Daniel Corrêa da Silva <danielcs80@gmail.com>
2024-10-30 07:30:00 -0300

    criação do ambiente de desenvolvimento

---
+++ src/controllers/clienteController.ts
@@ -0,0 +1,3 @@
+import {Cliente, clientes} from '../models/clienteModel'
+import { Request,Response } from 'express';
+import {v4 as uuidv4 } from 'uuid'
---
+++ src/models/clienteModel.ts
@@ -0,0 +1,9 @@
+interface Cliente {
+    id : string,
+    nome : string,
+    email : string
+}
+
+const clientes : Cliente[] = []
+
+export {Cliente, clientes}
